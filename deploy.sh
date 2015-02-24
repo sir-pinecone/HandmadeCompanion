2a506759b2be07bc67976b4b2b50d6567803d047
Jace Bennett <jace.bennett@gmail.com>
2015-02-23 20:55:36 -0500

    my conditional is wrong adding logging

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" == 'master' ]; then
+if [ "$TRAVIS_BRANCH" == 'master' ]
+then
     wput out ftp://$FTP_USER:$FTP_PASS@$FTP_HOST/ --less-verbose --reupload --dont-continue --basename=out/ | sed "s/=>.*:21//"
+else
+    echo "Declining to deploy branch $TRAVIS_BRANCH"
 fi
